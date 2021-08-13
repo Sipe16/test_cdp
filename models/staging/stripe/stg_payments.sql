d0ee440a54d1fe5337ef2dbff33135ee5d1f7ce3
simeonepepe.lavoro@gmail.com <simeonepepe.lavoro@gmail.com>
2021-08-13 17:56:13 +0000

    packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,11 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+
+    --la colonna amount è scritta in centesimi, convertirla in dollari
+   -- amount / 100 as amount_sql,                         --istruzione scritta in SQL
+    {{ cents_to_dollars('amount')}} as amount_jinja,    --istruzione scritta in Jinja
+    
+
     created as created_at
 from {{ source('stripe', 'payment') }}
